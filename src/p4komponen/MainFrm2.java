dc4005452c9bda00d2f6c46fd2c7961218d31852
Trisno Marsono <trisnomarsono@yahoo.com>
2017-07-27 18:32:06 +0700

    Tugas III Java3

---
+++ src/p4komponen/MainFrm2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p4komponen;
+
+/**
+ *
+ * @author kuman-pc
+ */
+public class MainFrm2 {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        FrmHuruf k = new FrmHuruf();
+        k.setVisible(true);
+    }
+    
+}
